a3fc21bdef13aef9a9fb3f083ee99d57ca0a3a9f
henry huynh <hlhuynh14@gmail.com>
2019-06-13 07:34:20 -0500

    made tables

--- MovieLibrary.js
+++ MovieLibrary.js
@@ -10,19 +10,46 @@ function SearchBy(category, searchQuery)
             break;
         case 'Genre':
             SearchByGenre(searchQuery)
-            break;    
-
+            break; 
+        case null:
+            SearchAll()
+            break;   
     }
 }
 function SearchByDirectorName(name)
 {
-
+    $.get( "http://localhost:50983/api/Movie", function( data ) {
+        $("#Table").empty();
+        $.each(data, function(key,value ) {
+            $("#Table").append(`<tr><td>${value.Title} </td><td>${value.Genre} </td><td>${value.DirectorName} </td></tr>`)            
+              }, "json" );
+})
 }
 function SearchByTitle(title)
 {
-    
+    $.get( "http://localhost:50983/api/Movie", function( data ) {
+        $("#Table").empty();
+        $.each(data, function(key,value ) {
+            $("#Table").append(`<tr><td>${value.Title} </td><td>${value.Genre} </td><td>${value.DirectorName} </td></tr>`)            
+              }, "json" );
+})
 }
 function SearchByGenre(genre)
 {
-    
+    $.get( "http://localhost:50983/api/Movie", function( data ) {
+        $("#Table").empty();
+        $.each(data, function(key,value ) {
+            $("#Table").append(`<tr><td>${value.Title} </td><td>${value.Genre} </td><td>${value.DirectorName} </td></tr>`)            
+              }, "json" );
+})
+}
+
+function SearchAll() 
+{    
+    $.get( "http://localhost:50983/api/Movie", function( data ) {
+        $("#Table").empty();
+        $.each(data, function(key,value ) {
+            $("#Table").append(`<tr><td>${value.Title} </td><td>${value.Genre} </td><td>${value.DirectorName} </td></tr>`)            
+              }, "json" );
+})   
 }
